e09e4b10d219c467ec9a9b089059705f27c1e0a1
tmhglnd <timohoogland@gmail.com>
2023-03-03 13:29:11 +0100

    registration page update

--- 2023/js/scripts-extension.js
+++ 2023/js/scripts-extension.js
@@ -6,18 +6,20 @@ let publishVenues = true;
 let publishSatellite = true;
 let publishTable = true;
 let publishMap = false;
+let publishRegistration = false;
 
 function makeMenu() {
 	document.body.innerHTML += `
 	<!-- Navigation-->
 	<nav class="navbar navbar-expand-lg navbar-black fixed-top" id="mainNav">
 		<div class="container px-4 px-lg-5">
-			<a class="navbar-brand" href="./index.html">ICLC 2023</a>
+			<a class="navbar-brand" href="./index.html">Registration & Tickets</a>
 			<button class="navbar-toggler navbar-toggler-right" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
 				<i class="fas fa-bars"></i>
 			</button>
 			<div class="collapse navbar-collapse" id="navbarResponsive">
 				<ul class="navbar-nav ms-auto">
+				${(publishRegistration)?`<li class="nav-item"><a class="nav-link" href="./index.html">Registration & Tickets</a></li>`:''}
 				${(publishTable)?`<li class="nav-item"><a class="nav-link" href="./timetable.html">Timetable</a></li>`:''}
 				${(publishVenues)?`<li class="nav-item"><a class="nav-link" href="./venues.html">Venues</a></li>`:''}
 				${(publishTravel)?`<li class="nav-item"><a class="nav-link" href="./travel-stay.html">Travel & Stay</a></li>`:''}
