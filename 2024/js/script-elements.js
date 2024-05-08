b3d8057db3203fd243dd2ed067c61ec178f78872
gohai <gottfried.haider@gmail.com>
2024-05-08 14:28:26 +0800

    Hardcode base url in navigation bar link

--- 2024/js/script-elements.js
+++ 2024/js/script-elements.js
@@ -21,24 +21,24 @@ function navigationBar() {
 	<!-- Navigation-->
 	<nav class="navbar navbar-expand-lg navbar-black fixed-top" id="mainNav">
 		<div class="container px-4 px-lg-5">
-			<a class="navbar-brand" href="./index.html">ICLC 2024</a>
+			<a class="navbar-brand" href="https://iclc.toplap.org/2024/">ICLC 2024</a>
 			<button class="navbar-toggler navbar-toggler-right" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
 				<i class="fas fa-bars"></i>
 			</button>
 			<div class="collapse navbar-collapse" id="navbarResponsive">
 				<ul class="navbar-nav ms-auto">
 				${(publishStream) ? `<li class="nav-item blink"><a class="nav-link" target="_blank" href="https://www.youtube.com/watch?v=FSBtvtxP008">Stream</a></li>` : ''}
-				${(publishCatalogue) ? `<li class="nav-item"><a class="nav-link" href="./catalogue/">Catalogue</a></li>` : ''}
-				${(publishProgram) ? `<li class="nav-item"><a class="nav-link" href="./program.html">Program</a></li>` : ''}
-				${(publishRegistration) ? `<li class="nav-item"><a class="nav-link" href="./registration.html">Registration</a></li>` : ''}
-				${(publishTickets) ? `<li class="nav-item"><a class="nav-link" href="./tickets.html">Tickets & Info</a></li>` : ''}
-				${(publishWorkshops) ? `<li class="nav-item"><a class="nav-link" href="./workshops.html">Workshops</a></li>` : ''}
-				${(publishMap) ? `<li class="nav-item"><a class="nav-link" href="./map.html">Map</a></li>` : ''}
-				${(publishVenues) ? `<li class="nav-item"><a class="nav-link" href="./venues.html">Venues</a></li>` : ''}
-				${(publishTravel) ? `<li class="nav-item"><a class="nav-link" href="./travel-stay.html">Travel & Stay</a></li>` : ''}
-				${(publishSatellite) ? `<li class="nav-item"><a class="nav-link" href="./satellite.html">Satellite Events</a></li>` : ''}
-				${(publishCall) ? `<li class="nav-item"><a class="nav-link" href="./open-call.html">Open Call</a></li>` : ''}
-				${(publishAbout) ? `<li class="nav-item"><a class="nav-link" href="./about.html">About</a></li>` : ''}
+				${(publishCatalogue) ? `<li class="nav-item"><a class="nav-link" href="https://iclc.toplap.org/2024/catalogue/">Catalogue</a></li>` : ''}
+				${(publishProgram) ? `<li class="nav-item"><a class="nav-link" href="https://iclc.toplap.org/2024/program.html">Program</a></li>` : ''}
+				${(publishRegistration) ? `<li class="nav-item"><a class="nav-link" href="https://iclc.toplap.org/2024/registration.html">Registration</a></li>` : ''}
+				${(publishTickets) ? `<li class="nav-item"><a class="nav-link" href="https://iclc.toplap.org/2024/tickets.html">Tickets & Info</a></li>` : ''}
+				${(publishWorkshops) ? `<li class="nav-item"><a class="nav-link" href="https://iclc.toplap.org/2024/workshops.html">Workshops</a></li>` : ''}
+				${(publishMap) ? `<li class="nav-item"><a class="nav-link" href="https://iclc.toplap.org/2024/map.html">Map</a></li>` : ''}
+				${(publishVenues) ? `<li class="nav-item"><a class="nav-link" href="https://iclc.toplap.org/2024/venues.html">Venues</a></li>` : ''}
+				${(publishTravel) ? `<li class="nav-item"><a class="nav-link" href="https://iclc.toplap.org/2024/travel-stay.html">Travel & Stay</a></li>` : ''}
+				${(publishSatellite) ? `<li class="nav-item"><a class="nav-link" href="https://iclc.toplap.org/2024/satellite.html">Satellite Events</a></li>` : ''}
+				${(publishCall) ? `<li class="nav-item"><a class="nav-link" href="https://iclc.toplap.org/2024/open-call.html">Open Call</a></li>` : ''}
+				${(publishAbout) ? `<li class="nav-item"><a class="nav-link" href="https://iclc.toplap.org/2024/about.html">About</a></li>` : ''}
 					<!--<li class="nav-item"><a class="nav-link" href="#signup">Contact</a></li>-->
 				</ul>
 			</div>
