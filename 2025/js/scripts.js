932f679090e038353142c7fd5d4dabc25a46cfc9
nik <nik@parkellipsen.de>
2025-03-20 18:58:47 +0100

    silence console error, redundant code

--- 2025/js/scripts.js
+++ 2025/js/scripts.js
@@ -30,13 +30,15 @@ window.addEventListener('DOMContentLoaded', event => {
     document.addEventListener('scroll', navbarShrink);
 
     // Activate Bootstrap scrollspy on the main nav element
+    /*
     const mainNav = document.body.querySelector('#mainNav');
     if (mainNav) {
         new bootstrap.ScrollSpy(document.body, {
             target: '#mainNav',
             offset: 74,
         });
-    };
+	};
+    */
 
     // Collapse responsive navbar when toggler is visible
     const navbarToggler = document.body.querySelector('.navbar-toggler');
